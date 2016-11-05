efd3796e4255dfd1f9b2603b6cc2e49600d83a90
djoysilva <joycesilva.web@gmail.com>
2016-11-05 18:42:06 -0200

    Created the first API with C#

---
+++ WebApplication3/WebApplication3/Controllers/ProductsController.cs
@@ -0,0 +1,34 @@
+﻿using ProductsApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+
+namespace ProductsApp.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        Product[] products = new Product[]
+        {
+            new Product { Id = 1, Name = "Candy", Category = "Food", Price = 10 },
+            new Product { Id = 2, Name = "Doll", Category = "Toys", Price = 301.75M },
+            new Product { Id = 3, Name = "Pc", Category = "Technology", Price = 1100.99M }
+        };
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return products;
+        }
+
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = products.FirstOrDefault((p) => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
